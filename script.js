fe3c6b96c01caacc5875f5acee001f7e40411375
kittu2karthik <71444538+kittu2karthik@users.noreply.github.com>
2025-10-07 19:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+	year = parseInt(year);
+	if(year % 100 === 0 ? year % 400 === 0 : year % 4 === 0){
+		return (366);
+	}
+	else{
+		return (365)
+	}
+}
